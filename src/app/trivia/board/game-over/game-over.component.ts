21ae90547d185522146dfed7965e3f4f3b497927
izik <izikdalal@gmail.com>
2020-10-03 14:22:05 +0300

    name validator

--- src/app/trivia/board/game-over/game-over.component.ts
+++ src/app/trivia/board/game-over/game-over.component.ts
@@ -10,6 +10,7 @@ export class GameOverComponent implements OnInit {
 
   @Input() score;
   @Output() save = new EventEmitter();
+  errMsg = '';
 
   constructor() {
   }
@@ -17,7 +18,16 @@ export class GameOverComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  private isValidName(name: string): boolean{
+    return new RegExp(/^[a-z0-9]+$/i).test(name);
+  }
+
   saveScore(name: string): void {
+    if (!this.isValidName(name)){
+      this.errMsg = 'Use alpha-numeric characters only';
+      return;
+    }
+    this.errMsg = '';
     const player: Player = {
       name,
       score: this.score
